f7a6f496d79fad57c6f1ea686786222772c1b888
mle <marie.lejeune@acsone.eu>
2022-03-23 11:14:53 +0100

    [UPD] mozaik_all : add mozaik_mail_rest to manifest

--- mozaik_all/__manifest__.py
+++ mozaik_all/__manifest__.py
@@ -47,6 +47,7 @@
         "mozaik_involvement",
         "mozaik_involvement_followup",
         "mozaik_involvement_rest",
+        "mozaik_mail_rest",
         "mozaik_mass_mailing_access_rights",
         "mozaik_mass_mailing_automation",
         "mozaik_mass_mailing_bounce_counter",
